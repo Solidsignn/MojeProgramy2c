c3a33bf09537760e8a45044da613581ad354c9b3
Solidsignn <120271641+Solidsignn@users.noreply.github.com>
2025-09-08 14:47:34 +0100

    Create KP2.cs

---
+++ KP2.cs
@@ -0,0 +1,25 @@
+//Z1
+//int a = int.Parse(Console.ReadLine());
+//if (a % 3 == 0)
+//    Console.WriteLine("TAK");
+//else
+//    Console.WriteLine("NIE");
+//Z2
+//int a = int.Parse(Console.ReadLine());
+//if (a % 17 == 0 & a > 100 & a < 1000)
+//    Console.WriteLine("TAK");
+//else
+//    Console.WriteLine("NIE");
+//Z3
+//int wiek = int.Parse(Console.ReadLine());
+//if (wiek > 17)
+//    Console.WriteLine("TAK");
+//else
+//    Console.WriteLine("NIE");
+//Z4
+//int waga = int.Parse(Console.ReadLine());
+//int limit = 20;
+//if (waga < limit)
+//    Console.WriteLine("TAK");
+//else
+//    Console.WriteLine("NIE");
