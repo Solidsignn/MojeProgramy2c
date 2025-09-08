e8ff2da7d23d60bf033fbc952920f8e3e01c605d
Solidsignn <120271641+Solidsignn@users.noreply.github.com>
2025-09-08 14:31:22 +0100

    Create KP1.cs

---
+++ KP1.cs
@@ -0,0 +1,44 @@
+//Z1
+//int a = int.Parse(Console.ReadLine());
+//int b = int.Parse(Console.ReadLine());
+//int c = a + b;
+//int d = c * c;
+//Console.WriteLine(d);
+//Z2
+//int a = int.Parse(Console.ReadLine());
+//int b = int.Parse(Console.ReadLine());
+//int c = a + b;
+//int d = c * c;
+//Console.WriteLine(d);
+//Z3
+//int a = int.Parse(Console.ReadLine());
+//int b = int.Parse(Console.ReadLine());
+//int c = a - b;
+//int d = c * c * c;
+//Console.WriteLine(d);
+//Z4
+//int a = int.Parse(Console.ReadLine());
+//int b = int.Parse(Console.ReadLine());
+//int c = int.Parse(Console.ReadLine());
+//int d = a * b * c;
+//Console.WriteLine(d);
+//Z5
+//int a = int.Parse(Console.ReadLine());
+//int b = int.Parse(Console.ReadLine());
+
+
+//double d = 2 * (a + b) / 5;
+//double e = (a + b) * 0.4;
+
+//Console.WriteLine(d);
+//Console.WriteLine(e);
+//Z6
+//int brutto = int.Parse(Console.ReadLine());
+
+//double d = brutto/1.23;
+//Console.WriteLine(d);
+//Z7
+//int a = int.Parse(Console.ReadLine());
+//int b = int.Parse(Console.ReadLine());
+//double d = a%b;
+//Console.WriteLine(d);
