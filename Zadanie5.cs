80d782979dc71454d964d1fa0d11145d0985ba7e
Solidsignn <120271641+Solidsignn@users.noreply.github.com>
2025-09-08 14:15:49 +0100

    Create Zadanie5.cs

---
+++ Zadanie5.cs
@@ -0,0 +1,9 @@
+int a = int.Parse(Console.ReadLine());
+int b = int.Parse(Console.ReadLine());
+
+
+double d = 2 * (a + b) / 5;
+double e = (a + b) * 0.4;
+
+Console.WriteLine(d);
+Console.WriteLine(e);
