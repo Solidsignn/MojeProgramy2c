f41b19d51a0de0a6639f4c5a4e3a56c5deb0304e
Solidsignn <120271641+Solidsignn@users.noreply.github.com>
2025-09-09 16:04:45 +0200

    Create KP3.cs

---
+++ KP3.cs
@@ -0,0 +1,20 @@
+//Z1
+//for (int i = 1; i <= 9999; i++)
+//{
+//    Console.WriteLine((i * i * i) + 3);
+//}
+//Z2
+//for (int i = 100; i < 1000; i++)
+//{
+//    if (i % 15 == 0) Console.Write(i + " ");
+//}
+//Z3
+//int n = int.Parse(Console.ReadLine());
+//for (int i = 1; i <= n; i++)
+//{
+//    if (n % i == 0) Console.Write(i + " ");
+//}
+//Z4
+//int suma = 0;
+//for (int i = 10; i <= 99; i++) suma += i;
+//Console.WriteLine(suma + "\n");
