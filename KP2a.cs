4d15d3106dfa51927578a873b50a03f35ca621b7
Solidsignn <120271641+Solidsignn@users.noreply.github.com>
2025-09-11 14:10:00 +0100

    Create KP2a.cs

---
+++ KP2a.cs
@@ -0,0 +1,33 @@
+//Z1
+//int a = int.Parse(Console.ReadLine());
+//int b = int.Parse(Console.ReadLine());
+//
+//if ((a + b) % 2 == 0)
+//    Console.WriteLine("TAK");
+//else
+//    Console.WriteLine("NIE");
+//Z2
+//int a = int.Parse(Console.ReadLine());
+//int g = int.Parse(Console.ReadLine());
+//
+//if ((a + g) / 2 > Math.Sqrt(a + g))
+//    Console.WriteLine("TAK");
+//else
+//    Console.WriteLine("NIE");
+//Z3
+//int k = int.Parse(Console.ReadLine());
+//int l = int.Parse(Console.ReadLine());
+//int m = int.Parse(Console.ReadLine());
+//if (k == l & k == m)
+//    Console.WriteLine("NIE");
+//    k = 1;
+//    l = 2;
+//    m = 3;
+//if (k != l & k != m & l != m)
+//    Console.WriteLine("NIE");
+//if (k == l)
+//    Console.WriteLine("TAK" + " " + k + " " + l);
+//if (k == m)
+//    Console.WriteLine("TAK" + " " + k + " " + m);
+//if (l == m)
+//   Console.WriteLine("TAK" + " " + l + " "+ m);
